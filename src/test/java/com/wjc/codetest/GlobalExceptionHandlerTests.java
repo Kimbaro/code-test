00ea7212e8e9df2f9746f53b692f826f8e093f39
Kimbaro <azxcv1329@gmail.com>
2026-01-19 02:19:20 +0900

    :zap: 테스트코드 작성부 추가

---
+++ src/test/java/com/wjc/codetest/GlobalExceptionHandlerTests.java
@@ -0,0 +1,15 @@
+package com.wjc.codetest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@SpringBootTest
+class GlobalExceptionHandlerTests {
+
+}
